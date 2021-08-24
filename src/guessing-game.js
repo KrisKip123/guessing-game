f448e4f4fc2139ca919e1e248a6511c42edfde03
Kirill Lipnitskiy <kriskip12393@gmail.com>
2021-08-24 17:41:15 +0300

    feat: gg

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.max = null;
+        this.min = null;
+        this.middle = null;
+    }
+
+
 
     setRange(min, max) {
-
+        this.max = max;
+        this.min = min;
     }
 
     guess() {
-
+        return this.middle = Math.round((this.max - this.min) / 2 + this.min);
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
+
 }
 
 module.exports = GuessingGame;
